d6fc6731f126f3e6ba61e1045ec53f099543c15a
Aerated-Zephyr <aryan.hassan2008@gmail.com>
2025-07-23 22:37:54 +0600

    something

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,16 +3,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          a.
+          AAAA
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
