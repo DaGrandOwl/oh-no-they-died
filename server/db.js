b16a2d9a09ab477459dccbee980f4a3f354647ba
DaGrandOwl <dagrandowl@gmail.com>
2025-07-29 12:35:34 +0600

    MySQL cloud DB

--- server/db.js
+++ server/db.js
@@ -6,5 +6,9 @@ export const db = await mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
-});
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT ? parseInt(process.env.MYSQL_PORT, 10) : 3306,
+  ssl: process.env.MYSQL_SSL === 'true' ? {
+    rejectUnauthorized: false,
+  } : false,
+});   
